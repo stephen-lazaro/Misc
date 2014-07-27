addf4da1b3c7b5d41c62ae78153c7c5802b57594
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-07-26 23:59:59 -0700

    First Draft on PascalNode
    Is Done. Now we should test it and do some checks. But it’s like
    midnight and ya boy is tired.

--- pascal.py
+++ pascal.py
@@ -5,9 +5,10 @@ The tactic will be to reduce to a tree like structure. Each tree will have a rig
 
 class PascalNode:
   def __init__(self, lefparen, riparen):
-    if lefparen is None:
+    self.lefparen, self.riparen = lefparen, riparen
+    if self.lefparen is None:
       self.value = 1
-    elif riparen is None:
+    elif self.riparen is None:
       self.value = 1
     else:
       self.value = self.lefparen.value + self.riparen.value
@@ -15,9 +16,25 @@ class PascalNode:
   def rikin(self):
     if self.riparen is None:
       return PascalNode(self, None)
-    return PascalNode(self, self.riparen.rikin)
+    return PascalNode(self, self.riparen.rikin())
 
   def lefkin(self):
     if self.lefparen is None:
       return PascalNode(None, self)
-    return PascalNode(self.lefparen.lefkin, self)
+    return PascalNode(self.lefparen.lefkin(), self)
+
+#What I want is a way to measure my depth, but this is hard to do.#
+#Simpler is to just use r and c as parameters on a while loop.#
+
+def findMyNode(r, c):
+  current_node = PascalNode(Null, Null)
+  while r != 0 and c !=0:
+    if r == 1:
+      current_node = PascalNode(currentnode.riparen, currentnode.riparen.rikin().riparen)
+      c -= 1
+      if r == 1 and c == 1:
+        current_node = PascalNode(currentnode.lefparen.lefkin(), currentnode)
+    else:
+      current_node = PascalNode(None, currentnode)
+      r -= 1
+  return current_node
