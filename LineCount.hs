6b89d57e7fb6f18c2afbd7f6c465197f4cc14768
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-07-26 22:21:48 -0700

    Last First Attempt
    We wrote a function that returns the last element of a list. It appears
    to compile. It needs to be tested for semantic errors.

--- LineCount.hs
+++ LineCount.hs
@@ -1,2 +1,2 @@
-main = interact wordCount
-     where wordCount input = show (length (lines input)) ++ "\n"
+main = interact lineCount
+     where lineCount input = show (length (lines input)) ++ "\n"
