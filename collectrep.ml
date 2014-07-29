3c4416b2374838018309313c5fa869f27c1bf370
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-07-28 18:31:00 -0700

    collectrep.ml
    Used exercise answer. Don’t understand it too well I’m afraid. I just
    can’t quite figure out what the in clause is for. It must be because
    we’re adding all of our collected lists on backwards, so then we feed
    it into reverse… but how that ‘in’ works and why I do not know.

--- collectrep.ml
+++ collectrep.ml
@@ -1,5 +1,11 @@
 let collectrep listical =
-  let rec crepaux acc =
-    |a :: (b :: listical) -> if a = b then crepaux (a :: acc) listical else acc :: crepaux [] listical
-    |[] -> []
+  let rec crepaux current acc = function
+    |[] -> [] (*Only if original list is empty *)
+    |[x] -> (x :: current) :: acc
+    |a :: (b :: listical) -> if a = b
+                            then crepaux (a :: current) acc listical
+                            else crepaux [] ((a :: current) :: acc) listical in
+                               List.rev (crepaux [] [] listical) 
 ;;
+
+collectrep ["a";"a";"b";"c";"c";"c";"d";"e";"e";"e";"e"]
