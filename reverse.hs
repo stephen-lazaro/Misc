8eb7b313a392ad1a32cc03f948c4c6be247a218a
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-07-30 09:27:49 -0700

    Update reverse.hs

--- reverse.hs
+++ reverse.hs
@@ -1,2 +1,5 @@
-reversus [] acc = acc
-reversus (x:xs) acc = reversus xs (x : acc)
+reverse listical =
+  let reversus =
+    reversus [] acc = acc
+    reversus (x:xs) acc = reversus xs (x : acc)
+  in reversus listical []
