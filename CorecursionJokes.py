a9d17487ef2d8c3f1c2a66fd9ddbc15208e5d244
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-07 14:29:36 -0700

    Update CorecursionJokes.py

--- CorecursionJokes.py
+++ CorecursionJokes.py
@@ -1,17 +1,50 @@
-#Just trying to play with corecursion and generators a little.
 def fact():
   n,f = 0,1
   while True:
   	yield f
   	n,f = n + 1, (n+1)*f
 
-#Now this gets us a particular factorial from the stream
-class factYielder():
-  def __inite__(self, x):
+#Now this gets us a particular item from a stream. (Interestingly, we also get all the ones leading up. Not sure why.)
+#Of course, all this would be better done using the itertools module. But I'm just trying to get a feel for the stuff.
+class genYielder():
+  def __init__(self, gen, x):
     self.a = x
+    self.ite = iter(gen())
+    self.value = 0
     
   def get(self):
-    return iter(fact())[self.a - 1]
+    i = 0
+    while i < self.a + 1:
+      try:
+        fax = next(self.ite)
+        print(fax)
+      except StopIteration:
+        self.value = fax
+        return fax
+      finally:
+      	i += 1
+    self.value = fax
+    return fax
 
-vaz = factYielder(6)
-print(vaz.get())
+vaz = genYielder(fact, 6)
+vaz.get()
+print(vaz.value)
+
+def fib():
+  n,m = 0,1
+  while True:
+    yield n
+    n,m = m, n+m
+
+vaz = genYielder(fib, 27)
+vaz.get()
+print(vaz.value)
+
+def naturals():
+	n,m=0,1
+	while True:
+		yield n
+		n,m = m,m+1
+
+evens = (2n for n in naturals)
+squares = (n*n for n in naturals)
