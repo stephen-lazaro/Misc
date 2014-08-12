a0e915b40a797f7d66113ecd00919bac45eddce0
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-11 23:25:53 -0700

    Quick and Merge Sorts
    Don’t compile. Errors are:
    sorting.erl:23: syntax error before: '.'
    sorting.erl:2: function mergesorts/1 undefined
    sorting.erl:8: call to local/imported function len/1 is illegal in guard
    sorting.erl:4: Warning: function len/1 is unused
    sorting.erl:5: Warning: variable 'Hd' is unused
    sorting.erl:7: Warning: function splith/3 is unused
    error

--- sorting.erl
+++ sorting.erl
@@ -1,10 +1,23 @@
 -module(sorts).
--export([quicksorts/1, mergesorts/1])
+-export([quicksorts/1, mergesorts/1]).
 
-splith([], _) -> [];
-splith(Lz, Acc, Loc) when (len(Lz) / 2) =:= Loc -> [[Acc|[]] | [splith(Lz, [], Loc + 1)]];
+len([]) -> 0;
+len([Hd|Lz]) -> 1 + len(Lz).
+
+splith([], _, _) -> [];
+splith(Lz, Acc, Loc) when (len(Lz) / 2) =:= Loc -> [[Acc|[]] | [Lz]];
 splith([Hd,Lz], Acc, Loc) -> splith(Lz, [Acc | Hd], Loc + 1).
 
-splitpiv
+splitpiv(Left, Right, _, [])                           -> [[Left] | [Right]];
+splitpiv(Left, Right, Pivot, [Hd|Lz]) when Hd > Pivot  -> splitpiv(Left, [Hd | Right], Pivot, Lz);
+splitpiv(Left, Right, Pivot, [Hd|Lz]) when Hd =< Pivot -> splitpiv([Hd | Left], Right, Pivot, Lz).
 
-quicksort(Lz) -> 
+quicksorts([])                 -> [];
+%quicksorts([A, B]) when A =< B -> [A, B];
+%quicksorts([A, B]) when A > B  -> [B, A];
+quicksorts([[Left], [Right]])  -> [quicksorts(Left) | quicksorts(Right)];
+quicksorts([Hd|Lz])            -> quicksorts(splitpiv([], [], Hd, Lz)). 
+
+mergesorts([])      -> [];
+mergesorts([[Left], [Right]]) -> [mergesorts(Left) | mergesorts(Right)];
+mergesorts([Hd|Lz]) -> mergesorts(splith(Lz, [Hd|[]], 1).
