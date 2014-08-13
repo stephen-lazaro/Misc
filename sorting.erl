2fb4ee4502f0741170fa64056161b1664e69ea87
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-12 22:08:48 -0700

    Progress is made
    but merge sorts still does not work. I think the issue is logical. What
    I think lines two and three are saying is not what Erlang understands
    by those expressions. I’m working on it.

--- sorting.erl
+++ sorting.erl
@@ -1,14 +1,10 @@
 -module(sorting).
 -export([quicksorts/1, mergesorts/1]).
 
-%len([]) -> 0;
-%len([Hd|Lz]) -> 1 + len(Lz).
-
 quicksorts([])                 -> [];
 quicksorts([[Left], [Right]])  -> [quicksorts(Left) | quicksorts(Right)];
 quicksorts([Hd|Lz])            -> quicksorts([X||X <- Lz, X =< Hd]) ++ [Hd] ++ quicksorts([Y||Y <- Lz, Y > Hd]). 
 
-
-mergesorts([])      -> [];
-mergesorts([Right,Left]) -> mergesorts(Right) ++ mergesorts(Left);
-mergesorts([Hd|Lz]) -> mergesorts(lists:split(lists:length([Hd|Lz]) div 2, [Hd|Lz])).
+mergesorts([])           -> [];
+mergesorts({Left,Right}) -> mergesorts(lists:split(length(Left) div 2, Left)) ++ mergesorts(lists:split(length(Right) div 2, Right));
+mergesorts(Lz)           -> mergesorts(lists:split(length(Lz) div 2, Lz)).
