cca345717a597a5b4c1303bd1fe32879ffa3632e
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-12 21:15:37 -0700

    Quicksort Works!
    But merge sort still has some problems. Mainly, there is a runtime
    error on the last line. This shouldn’t seems surprising, since frankly,
    it fucking looks wrong.

--- range.erl
+++ range.erl
@@ -1,5 +1,7 @@
 -module(range).
 -export([ranger/1]).
 
+%returns list descending from the argument of numbers less than the argument.
+%One would really want this list reversed but that's not hard so...
 ranger(0) -> [0];
 ranger(N) -> [N | ranger(N - 1)].
--- sorting.erl
+++ sorting.erl
@@ -1,31 +1,14 @@
--module(sorts).
+-module(sorting).
 -export([quicksorts/1, mergesorts/1]).
 
-len([]) -> 0;
-len([Hd|Lz]) -> 1 + len(Lz).
-
-splith([], _, _) -> [];
-splith(Lz, Acc, Loc) when (len(Lz) div 2) =:= Loc -> [[Acc|[]] | [Lz]];
-splith([Hd,Lz], Acc, Loc) -> splith(Lz, [Acc | Hd], Loc + 1).
-
-splitpiv(Left, Right, _, [])                           -> [[Left] | [Right]];
-splitpiv(Left, Right, Pivot, [Hd|Lz]) when Hd > Pivot  -> splitpiv(Left, [Hd | Right], Pivot, Lz);
-splitpiv(Left, Right, Pivot, [Hd|Lz]) when Hd =< Pivot -> splitpiv([Hd | Left], Right, Pivot, Lz).
-
-%Something tells me both of those are better written in list comprehensions, (which I didn't even know about at the time).
-splitpiv_list_comp([], _) -> [];
-splitpiv_list_comp(Lz, Pivot) -> [[X| X <- Lz, X =< Pivot] | [[Y| Y <- Lz, Y > Pivot]]].
-
-%This one is a BIF!
-splith_bif(Lz) -> lists:split(length(Lz) div 2, Lz).
-
+%len([]) -> 0;
+%len([Hd|Lz]) -> 1 + len(Lz).
 
 quicksorts([])                 -> [];
-%quicksorts([A, B]) when A =< B -> [A, B];
-%quicksorts([A, B]) when A > B  -> [B, A];
 quicksorts([[Left], [Right]])  -> [quicksorts(Left) | quicksorts(Right)];
-quicksorts([Hd|Lz])            -> quicksorts(splitpiv_list_comp(Lz, Hd)). 
+quicksorts([Hd|Lz])            -> quicksorts([X||X <- Lz, X =< Hd]) ++ [Hd] ++ quicksorts([Y||Y <- Lz, Y > Hd]). 
+
 
 mergesorts([])      -> [];
-mergesorts([[Left], [Right]]) -> [mergesorts(Left) | mergesorts(Right)];
-mergesorts([Hd|Lz]) -> mergesorts(splith(Lz, [Hd|[]], 1).
+mergesorts([Right,Left]) -> mergesorts(Right) ++ mergesorts(Left);
+mergesorts([Hd|Lz]) -> mergesorts(lists:split(lists:length([Hd|Lz]) div 2, [Hd|Lz])).
