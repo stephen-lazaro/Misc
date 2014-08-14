48061764c3a12ff3cc9b796683a7b17d566a7033
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-13 22:50:48 -0700

    Coroutines
    Compiles just fine! But it stops at 53 without terminating properly and
    I’m not sure why.

--- coroutines.erl
+++ coroutines.erl
@@ -1,19 +1,22 @@
 -module(coroutines).
--export([getNaturals/0]).
+-export([getNaturals/0, receiveNaturals/0, naturals/1]).
 
 getNaturals() ->
   Pid = spawn(coroutines, naturals, [53]),
   Pid ! {self(), start},
-  recieveNaturals(Pid).
+  receiveNaturals(),
+  Pid ! stop.
 
-recieveNaturals(Pid) ->
+receiveNaturals() ->
   receive
-    {_, 0} -> io:format("~w~n", [0]);
-    {Pid, N} -> io:format("~w~n", [N]), Pid ! next
+    {_, 0}   -> io:format("~w~n", [0]);
+    {Spid, N} -> io:format("~w~n", [N]), Spid ! next, receiveNaturals()
   end.
 
 naturals(N) ->
   receive
+    {From, start}-> From ! {self(), N}, naturals(N - 1);
     {From, 0}    -> From ! {self(), 0};
-    {From, next} -> From ! {self(), N}, naturals(N - 1)
+    {From, next} -> From ! {self(), N}, naturals(N - 1);
+    stop         -> true
   end.
