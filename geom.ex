3a1c8997bd9fd1a3794d56d7f9646839513c90ce
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-15 15:31:07 -0700

    Update geom.ex
    Forgot atom syntax is a little different in Elixir vs. Erlang.

--- geom.ex
+++ geom.ex
@@ -6,8 +6,8 @@ defmodule Geom do
   #I don't actually know if elixir allows this whole guards in function declaration thing
   def area(Shape, X, Y) when X >= 0, Y >= 0 do
     case Shape do
-      triangle -> area(X, Y) div 2
-      ellipse  -> area(X, Y) * math:pi
+      :triangle -> area(X, Y) div 2
+      :ellipse  -> area(X, Y) * math:pi
       _        -> area(X, Y)
     end
   end
