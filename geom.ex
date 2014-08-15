7175e5e8d70167cce5848809e744a8f9a44eebe8
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-15 15:29:49 -0700

    Update geom.ex

--- geom.ex
+++ geom.ex
@@ -3,4 +3,11 @@ defmodule Geom do
     X * Y
   end
   
-  def area
+  %I don't actually know if elixir allows this whole guards in function declaration thing
+  def area(Shape, X, Y) when X >= 0, Y >= 0 do
+    case Shape do
+      triangle -> area(X, Y) div 2
+      ellipse  -> area(X, Y) * math:pi
+      _        -> area(X, Y)
+    end
+  end
