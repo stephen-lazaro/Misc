359c9b4bc3cca49b2865e020fb1aac0adfea32ab
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-19 15:29:16 -0700

    Update mult_returnval.go

--- mult_returnval.go
+++ mult_returnval.go
@@ -14,5 +14,5 @@ func somprodiff(a int, b int) (sum int, product int, difference int) {
     sum = a + b
     product = a * b
     difference = a - b
-    return sum, product, difference
+    return //we could put the names here, but it's a better example of the named returns not to write them
 }
