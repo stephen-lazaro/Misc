409fdf2ee95cb6817adc134918be781ee824dad2
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-19 20:20:00 -0700

    Naive Prime test done
    But the factors function isn’t working, infinite recursion
    unfortunately.

--- arithz.erl
+++ arithz.erl
@@ -1,6 +1,6 @@
 % Arithmetic problems as seen in 99 Problems Solved in Lisp (or OCaml).
 -module(arithz).
--export([gcd/2, coprime/2, phi/1, euclid/2]).
+-export([gcd/2, coprime/2, phi/1, euclid/2, naiprime/1, factors/1]).
 
 %Finds greatest common divisor using djikstra's method
 gcd(N, M) when N =:= M -> N;
@@ -26,4 +26,14 @@ phaux(_, 0, Acc) -> Acc + 1;
 phaux(N, M, Acc) -> phaux(N, M - 1, Acc + coprime_num(M,N)).
 phi(N) -> phaux(N, N, 0).
 
-%
+%Naive prime test, way too inefficient for real use
+naiprimer(_, 1)                    -> true;
+naiprimer(N, M) when N rem M =:= 0 -> false;
+naiprimer(N, M) when N rem M =/= 0 -> naiprimer(N, M-1).
+naiprime(N) -> naiprimer(N, N - 1).
+
+%Get the list of prime factors
+facaux(_, 0, Acc)                    -> Acc;
+facaux(N, M, Acc) when N rem M =/= 0 -> facaux(N, M - 1, Acc);
+facaux(N, M, Acc) when N rem M =:= 0 -> facaux(N rem M, M, [M | Acc]).
+factors(N) -> facaux(N, N-1, []).
