8f8b5285d814d3151d1ae6188cb52bdaf9cbb304
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-19 19:12:35 -0700

    Debugging scat.go
    Plus another test for garbler.

--- scat.go
+++ scat.go
@@ -11,55 +11,39 @@ func main() {
     var first, second string
     fmt.Println("What two files do you want to concatenate? Separate with spaces, please.")
     fmt.Scanln(&first, &second)
-    firstFile := openFile(first)
-    secondFile := openFile(second)
+    firstFileReader := openFile(first)
+    secondFileReader := openFile(second)
     acc := ""
     dest := makeNewFile(&first, &second)
-    copyDirect(dest, firstFile, secondFile)
+    readInto(&dest, &firstFileReader)
+    readInto(&dest, &secondFileReader)
 }
 
+func readInto(destin *bufio.Writer, sourz *bufio.Reader) { }
+
 func writeResult(concatd bufio.Writer, material *string) {
-    success, err := concatd.writeString(*string)
+    success, err := concatd.WriteString(*material)
     if err != nil {
         fmt.Println("Writing failed!")
         os.Exit(1)
     }
 }
 
-func copyDirect(dest *os.File, srcA *os.File, srcB *os.File) int {
-    defer srcA.Close()
-    defer srcB.Close()
-    defer dest.Close()
-    aReader := bufio.NewReader(*srcA)
-    bReader := bufio.NewReader(*srcB)
-    destReader := bufio.NewWriter(*dest)
-    _, err := io.Copy(*dest, *srcA)
-    if err != nil {
-        fmt.Println("First file copy failed!")
-        return 1
-    }
-    _, err = io.Copy(*dest, *srcB)
-    if err != nil {
-        fmt.Println("Second file copy failed!")
-        return 2
-    }
-    return 0
-}
-
 func openFile(filename string) bufio.Reader {
-    filehand, err := os.Open(&filename)
+    filehand, err := os.Open(filename)
     if err != nil {
         fmt.Println("Are you sure that was a file?")
         os.Exit(-1)
     }
     defer filehand.Close()
-    filereader = bufio.NewReader(filehand)
+    filereader := bufio.NewReader(filehand)
     return filereader
 }
 
-func makeNewFile(filenameA *string, filenameB *string) os.File {
+func makeNewFile(filenameA *string, filenameB *string) bufio.Writer {
     newName := *filenameA + "&" + *filenameB + ".txt"
     newFile := os.Create(newName)
-    return newFile
+    newFileWriter := bufio.NewWriter(newFile)
+    return newFileWriter
 }
 
