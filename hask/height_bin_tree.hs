87fdc80cdde901aefe3b7e8d7ff2868886b81bb7
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-22 13:33:27 -0700

    Update height_bin_tree.hs

--- hask/height_bin_tree.hs
+++ hask/height_bin_tree.hs
@@ -4,13 +4,11 @@ max n m =
   |n < m     = m
   |otherwise = n
 
-data Tree a = Node a (Tree a) (Tree a)
+data Tree a = Node a { lefkin :: (Tree a), rikin :: (Tree a) }
               |Empty
+               deriving (Show)
 
-lefkin :: Tree a -> Tree a
-lefkin tree = Tree m _
 
-rikin tree = Tree _ m
 
 height :: Tree a -> Int
 height Empty = 0
