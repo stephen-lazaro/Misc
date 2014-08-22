a774decf655b3f5122c4ae23413b8bb79952a3b9
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-22 14:28:07 -0700

    Update liszt.ex

--- erlvm/liszt.ex
+++ erlvm/liszt.ex
@@ -1,10 +1,16 @@
 defmodule liszt do
   def lastr(Lz) do
-    [Hd|Ls] = Lz
-    cond Ls == [] do
+    [Hd | Ls] = Lz
+    case Ls == [] do
       :true -> Hd
       :false -> lastr(Ls)
     end
+  end
+  def penlastr(Lz) do
+    [Hd | [Hdd | Ls] = Lz
+    case Ls == [] do
+      :true -> Hd
+      :false -> penlastr([Hdd
   end
 end
 #No good. This is a problem!
