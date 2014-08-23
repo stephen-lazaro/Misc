6f83740ef511aa823995dd9478d81d58286037dd
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-23 14:04:20 -0700

    web scrambler stuff
    adding a node data type to try and build a document object model for
    the web page.

--- golang/webscrambler.go
+++ golang/webscrambler.go
@@ -7,17 +7,102 @@ import (
   "os"
   )
 
+//Tree node data type
+type Node struct {
+    children []Node
+    content string
+}
+
+//HTML Tree node data type
+type HtmlNode struct {
+    children []HtmlNode
+    textcontent string
+    tag string
+}
+
+func (self *Node) Children() {
+    return (*self).children
+}
+
+func (self *Node) Childless() bool {
+    if (*self).children == nil {
+        return True
+    } else if (*self).children == {
+        return True
+    } else {
+        return False
+    }
+}
+
+func (self *Node) Content() {
+    return (*self).content
+}
+
+func (self *Node) Width() int {
+    return len((*self).children)
+}
+
+func (self *Node) Height() int {
+    if (*self).Childless() {
+        return 0
+    } else {
+        acc := []int
+        for child,_ := range (*self).Children() {
+            acc.append(1 + child.Height())
+        }
+        return max(acc)
+    }
+}
+
+func (self *Node) Append(toBeAdded ..*Node) {
+    for node, _ := range toBeAdded {
+        append((*self).children, *node)
+    }
+}
+
+func (self *HtmlNode) TagType() string {
+    return (*self).tag
+}
+
+//An interface for nodes
+type Node interface {
+    Children()
+    Content() string
+}
+
+//Implementation for Html Nodes
+func (self *HtmlNode) Children() {
+    return (*self).children
+}
+
+func (self *HtmlNode) Content() {
+    return (*self).textcontent
+}
+
+//EmptyNode shouldn't really do anything
+type EmptyNode struct {
+}
+
+func (*EmptyNode) Right() {
+}
+
+func (*EmptyNode) Left() {
+}
+
+func (*EmptyNode) Content() {
+}
+
 func main() {
     var url string
     getURL(&url)
     page := getPage(url)
     var contents string
     readFromPage(&contents)
-    tokenize(contents)
+    makeDOM(contents)
 }
 
-func tokenize(page *string) {
-    
+func makeDOM(page *string) *Node {
+
 }
 
 func getURL(storage *string) {
@@ -47,8 +132,8 @@ func readFromPage(destinate *string, request *io.ReadCloser) (int, error) {
             fmt.Println("Reading broken")
             os.Exit(1)
         }
-        for pos, char := range inputLine {
-            //Do something.
+        for _, char := range inputLine {
+            (*desinate) += char
         }
     }
 }
