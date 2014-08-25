3024b3af1f889e1c39d9d3bb9df30e7300d1d314
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-25 00:23:06 -0700

    Just some late night Erlangin

--- erlvm/liszt.erl
+++ erlvm/liszt.erl
@@ -1,6 +1,8 @@
-%%% As seen in 99 Problems in Scheme (or OCaml, if you swing that way)
+%% @author Stephen Lazaro <stephen.lazaro@berkeley.edu>
+%% @doc A small library of functions for lists. Most are already implemented in the lists module
+%% @reference As seen in 99 Problems in Scheme (or OCaml, if you swing that way)
 -module(liszt).
--export([lastr/1, pop2/1, penlast/1, penlastr/1, kthOf/2, len/1, reverse/1, palindrome/1, flattenr/1, compressr/1, packr/1, rler/1, mrler/1, derler/1, drler/1, dupe/1, dropr/2, splitr/2, slicer/3, rot/2]).
+-export([lastr/1, pop2/1, penlast/1, penlastr/1, kthOf/2, len/1, reverse/1, palindrome/1, flattenr/1, compressr/1, packr/1, rler/1, mrler/1, derler/1, drler/1, dupe/1, dropr/2, splitr/2, slicer/3, rot/2, rem_at/2, insert_at/3, orangez/2, crangez/2]).
 
 %Get's the last element of the list
 lastr([A])    -> A;
@@ -51,22 +53,30 @@ flattenr([Hd,_]) -> flattenr(Hd).
 
 %Removes redundancies in a lsit
 compressaux([], Acc, _) -> reverse(Acc);
-compressaux([Hd|Lz], Acc, Flag) when Hd =:= Flag -> compressaux(Lz, Acc, Flag);
-compressaux([Hd|Lz], Acc, Flag) when Hd =/= Flag -> compressaux(Lz, [Hd|Acc], Hd).
+compressaux([Hd|Lz], Acc, Flag) when Hd =:= Flag 
+  -> compressaux(Lz, Acc, Flag);
+compressaux([Hd|Lz], Acc, Flag) when Hd =/= Flag 
+  -> compressaux(Lz, [Hd|Acc], Hd).
 compressr([])      -> []; 
 compressr([Hd|Lz]) -> compressaux(Lz, [Hd], Hd).
 
 %Packs redundances into a sublist
-packaux([], Tacc, Acc, _)                          -> reverse([Tacc|Acc]);
-packaux([Hd|Lz], Tacc, Acc, Flag) when Hd =:= Flag -> packaux(Lz, [Hd|Tacc], Acc, Flag);
-packaux([Hd|Lz], Tacc, Acc, Flag) when Hd =/= Flag -> packaux(Lz, [Hd], [Tacc|Acc], Hd).
+packaux([], Tacc, Acc, _)
+  -> reverse([Tacc|Acc]);
+packaux([Hd|Lz], Tacc, Acc, Flag) when Hd =:= Flag 
+  -> packaux(Lz, [Hd|Tacc], Acc, Flag);
+packaux([Hd|Lz], Tacc, Acc, Flag) when Hd =/= Flag 
+  -> packaux(Lz, [Hd], [Tacc|Acc], Hd).
 packr([])      -> [];
 packr([Hd|Lz]) -> packaux([Hd|Lz], [], [], Hd).
 
 %Run length encoding of list (notice the second guard is strictly not necessary, but I include it to make the function self-documenting)
-rlaux([], Tacc, Acc, Flag)                       -> reverse([[Acc + 1,Flag]|Tacc]);
-rlaux([Hd|Lz], Tacc, Acc, Flag) when Hd =:= Flag -> rlaux(Lz, Tacc, Acc + 1, Flag);
-rlaux([Hd|Lz], Tacc, Acc, Flag) when Hd =/= Flag -> rlaux(Lz, [[Acc + 1, Flag]|Tacc], 0, Hd).
+rlaux([], Tacc, Acc, Flag)
+  -> reverse([[Acc + 1,Flag]|Tacc]);
+rlaux([Hd|Lz], Tacc, Acc, Flag) when Hd =:= Flag 
+  -> rlaux(Lz, Tacc, Acc + 1, Flag);
+rlaux([Hd|Lz], Tacc, Acc, Flag) when Hd =/= Flag 
+  -> rlaux(Lz, [[Acc + 1, Flag]|Tacc], 0, Hd).
 rler([])      -> [];
 rler([Hd|Lz]) -> rlaux([Hd|Lz], [], -1, Hd).
 
@@ -83,11 +93,16 @@ derlaux([[A,B]|Lz], Acc) -> derlaux([[A-1,B]|Lz],[B|Acc]).
 derler(Lz) -> derlaux(Lz, []).
 
 %Direct solution to the run length encoding problem; instead of constructing lists count things
-drleaux([], Flag, 1, Acc)                           -> reverse([Flag|Acc]);
-drleaux([], Flag, Count, Acc)                       -> reverse([[Count, Flag]|Acc]);
-drleaux([Hd|Lz], Flag, Count, Acc) when Hd =:= Flag -> drleaux(Lz, Flag, Count + 1, Acc);
-drleaux([Hd|Lz], Flag, 1, Acc)     when Hd =/= Flag -> drleaux(Lz, Hd, 1, [Flag|Acc]);
-drleaux([Hd|Lz], Flag, Count, Acc)                  -> drleaux(Lz, Hd, 1, [[Count, Flag]|Acc]).
+drleaux([], Flag, 1, Acc)
+  -> reverse([Flag|Acc]);
+drleaux([], Flag, Count, Acc)
+  -> reverse([[Count, Flag]|Acc]);
+drleaux([Hd|Lz], Flag, Count, Acc) when Hd =:= Flag 
+  -> drleaux(Lz, Flag, Count + 1, Acc);
+drleaux([Hd|Lz], Flag, 1, Acc)     when Hd =/= Flag 
+  -> drleaux(Lz, Hd, 1, [Flag|Acc]);
+drleaux([Hd|Lz], Flag, Count, Acc)                  
+  -> drleaux(Lz, Hd, 1, [[Count, Flag]|Acc]).
 drler([])      -> [];
 drler([Hd|Lz]) -> drleaux([Hd|Lz], Hd, 0, []).
 
@@ -114,5 +129,35 @@ slice_aux(0, M, T, [Hd|Lz], Acc)       -> slice_aux(0, M - 1, T, Lz, [Hd|Acc]);
 slice_aux(N, M, T, [_|Lz], Acc)        -> slice_aux(N - 1, M, T, Lz, Acc).
 slicer(N, M, Lz) -> slice_aux(N, M, N, Lz, []).
 
-%Rotates a list n slices to the left. This particular version only works if N < len(Lz)
+%Rotates a list n slices to the left.
 rot(N, Lz) -> slicer(N rem len(Lz), len(Lz), Lz) ++ slicer(0, N rem len(Lz), Lz).
+
+%Remove the nth element of a list, for now don't overindex.
+rem_at(N, Lz) -> rem_at(N, Lz, []).
+rem_at(0, [_|Lz], Acc) -> reverse(Acc) ++ Lz;
+rem_at(_, [], Acc) -> Acc;
+rem_at(N, [Hd|Lz], Acc) -> rem_at(N - 1, Lz, [Hd|Acc]).
+
+%Insert an element into a given position; I let it fail when list is empty
+insert_at(N, Elem, Lz) -> insert_at(N, Elem, Lz, []).
+insert_at(0, Elem, Lz, Acc) -> reverse(Acc) ++ [Elem|Lz];
+insert_at(N, Elem, [Hd|Lz], Acc) -> insert_at(N - 1, Elem, Lz, [Hd|Acc]).
+
+%Returns a range noninclusive i.e. open
+orangez(N, M) -> orangez(N, M, [], undef).
+orangez(N, M, Acc, _) when N > M -> orangez(N - 1, M, [(N - 1)|Acc], down);
+orangez(N, M, Acc, _) when N < M -> orangez(N + 1, M, [N|Acc], up);
+orangez(_, _, Acc, down) -> reverse(Acc);
+orangez(_, _, Acc, up)   -> reverse(Acc).
+
+%Returns a range inclusive i.e. closed
+crangez(N, M) when N < M -> orangez(N, M) ++ [M];
+crangez(N, M) when N > M -> [N|orangez(N, M)];
+crangez(N, _) -> [N].
+
+%%% Skipping a bunch of randomized element stuff here
+%%% We'll come back to it
+
+% Generate all combinations of a given length of elements taken from the list
+% First, a more general problem: generate all subsets
+subsets Lz -> 
---
+++ erlvm/war.erl
@@ -0,0 +1,9 @@
+%Chapter 8 Exercises Erlang
+%% @author Stephen Lazaro <stephen.lazaro@berkeley.edu>
+%% @doc A module to simulate a card game using processes
+%% @copyright Stephen Lazaro August 2014
+%% Not that anyone would want it anyway.
+
+-module(war).
+-export([]).
+
