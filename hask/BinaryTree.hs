238bc25c7db9413a16bdfcd76113c7d13d844a79
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-27 16:32:31 -0700

    Ugh
    Flattening is hard

--- hask/BinaryTree.hs
+++ hask/BinaryTree.hs
@@ -29,12 +29,13 @@ cbal_tree n
         | otherwise      = BinaryNode evenSubtree evenSubtree n
     where evenSubtree = cbal_tree (quot n 2)
           oddSubtree  = cbal_tree ((quot n 2) + 1)
+abz n
+  | n < 0     = -n 
+  | otherwise = n
 
-is_balanced :: BinaryNode a -> Boolean
+
+is_balanced :: BinaryNode a -> Bool
 is_balanced (BinaryNode a b _) = sideDifference <= 1
     where sideDifference = abz ((nodez a) - (nodez b))
-         where abz n
-             | n < 0      = -n
-             | otherwise  = n 
 
 --Let's check if a tree is symmetric
--- hask/liszt.hs
+++ hask/liszt.hs
@@ -40,9 +40,11 @@ palind lz = paux lz (reverse lz) True
     	  paux _ [] acc = False;
           paux (hd : lz) (f:other) acc = paux lz other (hd == f)
 
+--Trying to flatten a list
 data Dlist a = One a | Many [a]
 
-flattenr lz = let aux =
-  				aux ((One hd) : lst)  acc = aux lst hd:acc;
-  				aux ((Many hd) : lst) acc = aux lst ((aux hd) ++ acc)
-  			  in aux lz []
+flattenr lz = let flaux =
+				flaux [] acc = acc;
+  				flaux ((One hd) : lst)  acc = flaux lst hd:acc;
+  				flaux ((Many hd) : lst) acc = flaux lst ((flaux hd) ++ acc)
+  			  in flaux lz []
