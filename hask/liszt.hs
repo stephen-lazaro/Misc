6a8570019b2bf0a80c9091b8a88b2ce5313e7bdb
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-08-27 00:06:59 -0700

    debugging!

--- hask/liszt.hs
+++ hask/liszt.hs
@@ -1,29 +1,33 @@
-module liszt where
+module Liszt where
 
+--For many of these we use the Maybe data type for safety.
 --Returns the last element of a list
-lastr [] = False
-lastr (hd:[]) = hd
-lastr (hd:lz) = lastr lz
+lastr :: [Maybe a] -> Maybe a
+lastr [] = Nothing
+lastr (Just hd:[]) = Just hd
+lastr (Just hd:lz) = lastr lz
 
 --Returns the last two elements of a list
+penlast :: [a] -> [a]
 penlast [] = []
 penlast (hd:[]) = [hd]
 penlast (hd:(hdd:[])) = [hd, hdd]
 penlast (hd:lz) = penlast lz
 
 --Returns the kth element of a list
-at _ []    = False
-at 0 (hd:_) = hd
-at n (hd:lz) = at (n - 1) lz
+at :: Integer -> [Maybe a] -> Maybe a
+at _ []    = Nothing
+at 0 (Just hd:_) = Just hd
+at n (Just hd:lz) = at (n - 1) lz
 
 --Returns the number of elements in a list
+len :: [a] -> Integer 
 laux [] acc    = acc
 laux (hd:lz) acc = laux lz (acc + 1)
 len ls = laux ls 0
 
 --Reverses a list
-reversus [] acc = acc
-reversus (hd:lz) acc = reversus lz (acc ++ [hd])
-reversus lz = reversus lz []
-
---
+reversus :: [a] -> [a]
+reversus (hd:lz) = let aux [] acc = acc
+  	                   aux (f:rst) acc = aux rst (acc ++ [f])
+  	                in aux (hd:lz) []
