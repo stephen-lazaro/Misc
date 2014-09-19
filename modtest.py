32a943b95f25ca9cfdcd105e4814ca0a59670b64
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-09-19 12:28:38 -0700

    hmm
    seems pretty much equally fast, but I like the former better.

---
+++ modtest.py
@@ -0,0 +1,14 @@
+#modtest
+
+float_num = float(6789)
+
+for j in range(1, 500):
+	if float_num % j == 0:
+		print(False)
+		break
+'''
+for i in range(1, 500):
+	if (float_num / i).is_integer():
+		print(False)
+		break
+'''
