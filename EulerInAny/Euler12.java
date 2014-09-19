ac3ef5b0b63d4fa3cd6021c6b1069db94eb73f75
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-09-19 14:07:36 -0700

    Euler12 is so close...

---
+++ EulerInAny/Euler12.java
@@ -0,0 +1,104 @@
+//Euler12
+import java.lang.Math;
+import java.util.*;
+
+public class Euler12 {
+	static boolean debuga = false;
+	private static class Eratosthenes {
+		private boolean[] data;
+		private ArrayList<Integer> primes;
+		public Eratosthenes(int limit) {
+			this.data = new boolean[limit];
+			int sqrlim = (int) Math.sqrt(limit);
+			for (int i = 0; i < limit; i++) {
+				this.data[i] = true;
+			}
+			for (int j = 2; j < sqrlim; j++) {
+				if (this.data[j-2] == true) {
+					for (int k = 0; j*j + k*j < limit; k++) {
+						this.data[j*j + k*j - 2] = false;
+					}
+				}
+			}
+			primes = new ArrayList<Integer>();
+			for (int j=2; j < limit + 1; j++) {
+				if (this.data[j-2] == true) {
+					this.primes.add(j);
+				}
+			}
+		}
+		public boolean[] getData() {
+			return this.data;
+		}
+		public ArrayList<Integer> getPrimes() {
+			return this.primes;
+		}
+	}
+	private static class Divisors {
+		int divcount;
+		public Divisors(int n, Eratosthenes test) {
+			int rem = n;
+			int exponent;
+			ArrayList<Integer> primes = test.getPrimes();
+			this.divcount = 1;
+			int primeindex = 0;
+			for (int i = 0; i < primes.size(); i++) {
+				if (primes.get(i) * primes.get(i) > n) {
+					this.divcount = 2 * this.divcount;
+					return;
+				}
+				exponent = 1;
+				while (rem % primes.get(i) == 0) {
+					exponent++;
+					rem = rem / primes.get(i);
+				}
+				this.divcount = this.divcount * exponent;
+				if (rem == 1) {
+					return;
+				}
+			}
+		}
+		public int getCount() {
+			return this.divcount;
+		}
+	}
+	private static class Triangular {
+		int data;
+		public Triangular(int n) {
+			this.data = (n * n+1) / 2;
+		}
+		public int get() {
+			return this.data;
+		}
+	}
+
+	int data;
+	public Euler12() {
+		Eratosthenes test = new Eratosthenes(75000);
+		for (int i = 0; i < 10000; i++) {
+			Triangular item = new Triangular(i);
+			Divisors divs = new Divisors(item.get(), test);
+			if (divs.getCount() > 5) {
+				this.data = item.get();
+				return;
+			}
+		}
+	}
+	public int get() {
+		return this.data;
+	}
+	public static void main(String[] args) {
+		if (debuga == true) {
+			Eratosthenes test = new Eratosthenes(75000);
+			Divisors six = new Divisors(6, test);
+			System.out.format("%d\n", six.getCount());
+			Divisors twentyeight = new Divisors(28, test);
+			System.out.format("%d\n", twentyeight.getCount());
+			Divisors answer = new Divisors(76576500, test);
+			System.out.format("%d\n", answer.getCount());
+		} else {
+			Euler12 answer = new Euler12();
+			System.out.format("%d", answer.get());
+		}
+	}
+}
