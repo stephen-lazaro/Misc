c86fa3921d47fbd2ed0f532a20fcb70a445b958d
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-09-27 00:48:44 -0700

    Tested
    all golden

--- py/PermutationTest.py
+++ py/PermutationTest.py
@@ -1,19 +1,29 @@
 import itertools as it
 
+debug = True
+
 class PermutationsTest:
 	def __init__(self, a, b):
-		self.aset = Set(a)
-		self.bset = Set(b)
+		self.aset = set(a)
+		self.bset = set(b)
 		if (self.aset == self.bset and len(a) == len(b)):
-			this.val = True
+			self.val = True
 			return
-		this.val = False
+		self.val = False
 
 	def get(self):
 		return self.val
 
 	@staticmethod
-	def isPermute(cls, a, b):
-		if b in it.permutations(a):
+	def isPermute(a, b):
+		if tuple(b) in it.permutations(a):
 			return True
 		return False
+
+if debug == True:
+	woo = PermutationsTest("abcd", "dcba")
+	print(woo.get())
+	woo2 = PermutationsTest("abcd", "defg")
+	print(woo2.get())
+	print(PermutationsTest.isPermute("abcd", "dcba"))
+	print(PermutationsTest.isPermute("abcd", "defg"))
