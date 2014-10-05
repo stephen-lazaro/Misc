26c8450cf92ede0551241163ec60e5b8edebc7df
Stephen Lazaro <stephen.lazaro@gmail.com>
2014-10-04 19:35:42 -0700

    Works.
    Fixed! The key was that you have to compensate for the number of times
    that you actually do the copying across to know where t start in the
    initial string.

--- javac/WhiteSpacerIP.java
+++ javac/WhiteSpacerIP.java
@@ -2,18 +2,21 @@ public class WhiteSpacerIP {
 	char[] data;
 	public WhiteSpacerIP(String input, int size) {
 		this.data = input.toCharArray();
+		int k = 0;
 		for (int i = 0; i < this.data.length; i++) {
 			if (i > size) {
 				break;
 			}
 			if (this.data[i] == ' ') {
+				k += 2;
 				for (int j = 3; j + i < this.data.length; j++) {
-					this.data[i + j] = input.charAt(i - 2 + j);
+					this.data[i + j] = input.charAt(i - k + j);
 				}
 				this.data[i] = '%';
 				this.data[i + 1] = '2';
 				this.data[i + 2] = '0';
 				i = i + 2;
+				System.out.println(new String(this.data));
 			}
 		}
 	}
